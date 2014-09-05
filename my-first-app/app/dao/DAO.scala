bbccff9911474cfc60556339f272887397154f02
Pilou <Pilou@Pilou-PC>
2014-09-06 01:27:22 +0200

    - Make dao search not case sensitive

--- my-first-app/app/dao/DAO.scala
+++ my-first-app/app/dao/DAO.scala
@@ -11,15 +11,23 @@ object ItemDao extends GenericDao[Item, ItemsTable] {
   val tableQuery = TableQuery[ItemsTable]
   
   // Specific method
-	def sortAll(implicit session: JdbcBackend#Session): List[Item] = {
+  def sortAll(implicit session: JdbcBackend#Session): List[Item] = {
 	  return tableQuery.sortBy(_.name.asc.nullsFirst).list
-	}
+  }
   
   def getUserWithAll(userId: Long)(implicit session: JdbcBackend#Session): ItemWithAll= {
     val q = tableQuery.filter(_.id === userId)
     ItemWithAll.tupled(q.join(UserDao.tableQuery).on(_.userId === _.id).first)
   }
   
+  def search(query: String)(implicit session: JdbcBackend#Session): List[Item] = {
+    var q = tableQuery.filter(i => i.name.toLowerCase like "%"+query.toLowerCase+"%")
+    if (query forall Character.isDigit) {
+    	q = tableQuery.filter(i => (i.id === query.toLong) || (i.name.toLowerCase like "%"+query.toLowerCase+"%"))
+    }
+    q.sortBy(_.name.asc.nullsFirst).list
+  }
+
 }
 
 object TagDao extends GenericDao[models.Tag, TagsTable] {
@@ -27,6 +35,13 @@ object TagDao extends GenericDao[models.Tag, TagsTable] {
   // Initialize the tableQuery
   val tableQuery = TableQuery[TagsTable]
   
+  def search(query: String)(implicit session: JdbcBackend#Session): List[models.Tag] = {
+    var q = tableQuery.filter(i => i.name.toLowerCase like "%"+query.toLowerCase+"%")
+    if (query forall Character.isDigit) {
+    	q = tableQuery.filter(i => (i.id === query.toLong) || (i.name.toLowerCase like "%"+query.toLowerCase+"%"))
+    }
+    q.sortBy(_.name.asc.nullsFirst).list
+  }
 }
 
 object UserDao extends GenericDao[User, UsersTable] {
@@ -35,9 +50,9 @@ object UserDao extends GenericDao[User, UsersTable] {
   val tableQuery = TableQuery[UsersTable]
 
   def search(query: String)(implicit session: JdbcBackend#Session): List[User] = {
-    var q = tableQuery.filter(i => i.login like "%"+query+"%")
+    var q = tableQuery.filter(i => i.login.toLowerCase like "%"+query.toLowerCase+"%")
     if (query forall Character.isDigit) {
-    	q = tableQuery.filter(i => (i.id === query.toLong) || (i.login like "%"+query+"%"))
+    	q = tableQuery.filter(i => (i.id === query.toLong) || (i.login.toLowerCase like "%"+query.toLowerCase+"%"))
     }
     q.sortBy(_.login.asc.nullsFirst).list
   }
